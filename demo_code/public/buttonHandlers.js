d4e33d57fe9d2d530a3f1d594169ffd3f808c3f5
Emily-Kwok <ekwok101@gmail.com>
2024-04-13 13:23:42 -0400

    Working on HTML

--- demo_code/public/buttonHandlers.js
+++ demo_code/public/buttonHandlers.js
@@ -10,7 +10,6 @@ const DIR = "http://localhost:3000"
 function handleKeyUp(e) {
     if (e.which == ENTER) {
         console.log("KEYUP")
-        // console.log(document.documentURI)
         if( document.documentURI == (DIR + "/login") ) handleLoginButton()
         if( document.documentURI == (DIR + "/sign-up") ) handleSignInButton()
     }
@@ -18,55 +17,7 @@ function handleKeyUp(e) {
     e.preventDefault()
 }
 
-// function handleLoginButton() {
-//     let user = {}
-//     user.username = document.querySelector("#username").value
-//     user.password = document.querySelector("#password").value
-
-//     if( user && Object.keys(user).length != 0 ){
-//         const data = {text: user}
-//         fetch('http://localhost:3000/login',{
-//             method: 'POST',
-//             headers: {'Content-Type': 'application/json',},
-//             body: JSON.stringify(data),
-//         })
-//         .then((response) => response.json())
-//         .then((data) => {
-//             // console.log("User: ", user)
-//             console.log("Data: ", data)
-//             // let list = data.list
-//             // let userFound = false
-//             // console.log("List: ", data.list)
-//             // console.log("User Found: ", data.userFound)
-
-//             // console.log("WINDOW")
-
-//             // for( let x = 0 ; x < list.length ; x++ ){
-//             //     console.log(list[x])
-//             //     if( isUsersEqual(user, list[x]) ){
-//             //         userFound = true
-//             //         window.location.replace('/chatClient.html')     //Cannot find a way to get rid of '.html'
-//             //     }
-//             // }
-
-//             document.getElementById("errMessage").innerText = "Incorrect username or password. Please try again"
-//             document.getElementById("username").value = ""
-//             document.getElementById("password").value = ""
-
-//             // if( response.ok ){
-//             //     document.getElementById("errMessage").innerText = "Incorrect username or password. Please try again"
-//             //     document.getElementById("username").value = ""
-//             //     document.getElementById("password").value = ""
-//             // }
-//         })
-//         .catch((error) => {
-//             console.log('Error:', error)
-//         })
-//     }
-// }
-
 async function handleLoginButton() {
-    // while(true){
         const response = await fetch('/login')
         if( (document.getElementById("username").value == "" || document.getElementById("password").value == "" || document.getElementById("confirm-password").value == "")  ){
             document.getElementById("errMessage").innerText = "Form is incomplete. Please try again."
@@ -76,17 +27,7 @@ async function handleLoginButton() {
         }
         document.getElementById("username").value = ""
         document.getElementById("password").value = ""
-    // }
 }
-// handleLoginButton()
-
-// function handleSignInButton() {
-//     let user = {}
-//     user.username = document.querySelector("#username").value
-//     user.password = document.querySelector("#password").value
-  
-//     //
-// }
 
 async function handleSignInButton() {
     const response = await fetch('/sign-up')
--- demo_code/server.js
+++ demo_code/server.js
@@ -2,11 +2,22 @@ const express = require('express')
 const session = require('express-session')
 const app = express()
 const bodyParser = require('body-parser')
+const { exit } = require('process')
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
 const PORT = process.env.PORT || 3000
 const ROOT_DIR = '/public'
+
+const SECURITY_CODE = "0413";
+
+const MAX_MEMBERS_PER_GROUP = "6";
+
+const DROP_IN_FEE = "10";
+const MONTHLY_FEE = "30";
+const ANNUAL_FEE = "300";
+const PERSONAL_TRAINING_FEE = "50";
+const GROUP_FITNESS_FEE = "15";
 
 var listOfUsers = []
 var registeredUsers = [{username: "John", password: "1234"}]
@@ -76,13 +87,61 @@ app.get(['/', '/homepage'], (req, res) => {
     res.sendFile(__dirname + (ROOT_DIR + '/homepage.html'))
 })
 
-app.get('/login', (req, res) => {
-    console.log("Get Login")
-    res.sendFile(__dirname + (ROOT_DIR + '/login.html'))
+app.get('/login-portal', (req, res) => {
+    console.log("Get Login Portal")
+    res.sendFile(__dirname + (ROOT_DIR + '/login-portal.html'))
 })
 
-app.post('/login', (req, res) => {
-    console.log("Post Login")
+app.get('/signup-portal', (req, res) => {
+    console.log("Get SignUp Portal")
+    res.sendFile(__dirname + (ROOT_DIR + '/signup-portal.html'))
+})
+
+app.get('/authentication', (req, res) => {
+    console.log("Get Authentication")
+    res.sendFile(__dirname + (ROOT_DIR + '/employee-authentication.html'))
+})
+
+app.post('/authentication', (req, res) => {
+    console.log("Post Authentication")
+    console.log(req.body)
+    if( req.body.code == SECURITY_CODE ){
+        // res.sendFile(__dirname + (ROOT_DIR + '/chatClient.html'))
+        console.log("VALID")
+    }
+    else{
+        console.log("INVALID")
+    }
+})
+
+app.get('/member-login', (req, res) => {
+    console.log("Get Member Login")
+    res.sendFile(__dirname + (ROOT_DIR + '/member-login.html'))
+})
+
+app.post('/member-login', (req, res) => {
+    console.log("Post Member Login")
+    let user = {}
+    user.username = req.body.username
+    user.password = req.body.password
+    
+    for( let x = 0 ; x < registeredUsers.length ; x++ ){
+        // console.log(registeredUsers[x])
+        if( isUsersEqual(user, registeredUsers[x]) && ( user.username != "" && user.password != "" ) ){
+            req.session.user = true
+            res.sendFile(__dirname + (ROOT_DIR + '/chatClient.html'))   //Required?
+            res.redirect('/chat-server')
+        }
+    }
+})
+
+app.get('/employee-login', (req, res) => {
+    console.log("Get Employee Login")
+    res.sendFile(__dirname + (ROOT_DIR + '/employee-login.html'))
+})
+
+app.post('/employee-login', (req, res) => {
+    console.log("Post Employee Login")
     let user = {}
     user.username = req.body.username
     user.password = req.body.password
